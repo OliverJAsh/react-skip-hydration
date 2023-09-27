3667ac8ca6f4925f9e89eaf1d8697bc1e775a59a
Oliver Joseph Ash <oliverjash@gmail.com>
2023-09-27 10:54:55 +0100

    Remove before hydrate

--- client/index.tsx
+++ client/index.tsx
@@ -2,6 +2,8 @@ import { hydrateRoot } from 'react-dom/client';
 import { App } from '../shared/App';
 
 setTimeout(() => {
+    console.log('Remove');
+    document.querySelector('#A')?.remove();
     console.log('Hydrate');
     hydrateRoot(document.getElementById('root')!, <App />);
 }, 3000);
